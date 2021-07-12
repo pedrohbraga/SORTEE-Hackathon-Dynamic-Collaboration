fba4af4f4d225ac8ac64be9d0ab57efeb50f8cb8
Pedro Henrique Pereira Braga <pedrohenrique.pereirabraga@mail.concordia.ca>
2021-07-12 02:05:52 -0400

    Adding R setup, exercise 2 and updated material

---
+++ r_setup_for_developers.R
@@ -0,0 +1,55 @@
+### Setting up the RMarkdown document and its theme
+
+## Install rmdformats to obtain the theme used in this workshop
+
+install.packages("rmdformats")
+
+## Create a the .Rmd document with downcute
+
+# After the document is prepared, one must render it using `knitr`
+
+### Setting up the Github Action to automatically render the document
+
+install.packages("usethis")
+
+# `use_github_action()` allows us to fetch examples from the r-lib/actions repository
+
+usethis::use_github_action(name = "render-rmarkdown",
+                           save_as = "render-rmd-material.yaml",
+                           open = TRUE)
+
+
+
+### Preparation for Exercise 2 ####
+
+# Install and load tidyverse
+
+install.packages("tidyverse")
+
+library(tidyverse)
+
+# Install and load the penguin data set
+
+install.packages("palmerpenguins")
+
+library(palmerpenguins)
+
+# Load the penguins data set
+
+data(penguins)
+
+# Filter out the Chinstrap species from the data set
+
+penguins %>%
+  filter(species != "Chinstrap")
+
+# Write the filtered out data set
+
+write.csv(penguins %>%
+            filter(species != "Chinstrap"),
+          "docs/data/palmerpenguins_dataset.csv")
+
+# Write the entire data set containing all three penguin species
+
+write.csv(penguins,
+          "docs/data/palmerpenguins_updated_dataset.csv")
